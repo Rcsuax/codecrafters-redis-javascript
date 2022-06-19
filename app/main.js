7186953d1f3035ea88b690395ed20c35ff9d73ed
Reuben S <rcsuax@gmail.com>
2022-06-19 15:56:42 +0100

    test4

--- app/main.js
+++ app/main.js
@@ -5,13 +5,15 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer(socket => {
-  console.log(socket)
+  socket.on('data', data => {
+    console.log('DATA RECEIVED')
+    console.log(data.toString())
+  })
   socket.pipe(socket);
 });
 
 server.on('connection', socket => {
   console.log("CONNECTION MADE")
-  console.log(socket.bytesRead)
 })
 
 server.listen(6379, '127.0.0.1');
