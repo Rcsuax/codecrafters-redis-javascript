f2336cfc39caf0c3421f79e5020d363c5de7340e
Reuben S <rcsuax@gmail.com>
2022-06-19 16:53:59 +0100

    stage 6

--- app/main.js
+++ app/main.js
@@ -3,6 +3,8 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+const RedisStore = new Map()
+
 // Uncomment this block to pass the first stage
 const server = net.createServer(socket => {
   
@@ -10,7 +12,10 @@ const server = net.createServer(socket => {
     
     const input = data.toString()
     const array = input.split('\r\n')
+    
     const op = array[2] // [ '*2', '$4', 'echo', '$6', 'apples', '' ]
+    const key = array[4]
+    const value = array[6]
     
     switch(op) {
       case 'ping':
@@ -20,10 +25,14 @@ const server = net.createServer(socket => {
         socket.write(`+${array[4]}\r\n`)
         break;
       case 'set':
-        let key = array[4]
-        let value = array[6]
         console.log(`${op} = K: ${key} V: ${value}`)
+        
+        RedisStore.set(key, value)
         socket.write('+OK')
+        break;
+      case 'get':
+        let out = RedisStore.get(key)
+        socket.write(`+${out}`)
         break;
     }
 
