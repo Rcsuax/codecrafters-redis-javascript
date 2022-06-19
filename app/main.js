6c679ed6460ff4b0aca351d1602791100706dce9
Reuben S <rcsuax@gmail.com>
2022-06-19 17:15:11 +0100

    stage 7

--- app/main.js
+++ app/main.js
@@ -16,9 +16,7 @@ const server = net.createServer(socket => {
     const op = array[2] // [ '*2', '$4', 'echo', '$6', 'apples', '' ]
     const key = array[4]
     const value = array[6]
-    const ttl = array[8]
-
-    console.log(`ARRAY: ${array} , TTL VALUE: ${ttl}`)
+    const ttl = array[10]
     
     switch(op) {
       case 'ping':
